ca6e87026d470a479859d4594abc746ae131a5ed
L7D <smhjyh2009@gmail.com>
2015-04-01 11:38:37 +0900

    License add.

--- gamemode/schema/sh_schema.lua
+++ gamemode/schema/sh_schema.lua
@@ -1,3 +1,21 @@
+--[[
+< CATHERINE > - A free role-playing framework for Garry's Mod.
+Develop by L7D.
+
+Catherine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
 Schema.Name = "HL2RP"
 Schema.Author = "L7D"
 Schema.Title = "Half - Life 2 Role Play"
@@ -10,13 +28,15 @@ catherine.util.Include( "sh_commands.lua" )
 catherine.util.Include( "sv_schema.lua" )
 catherine.util.Include( "cl_schema.lua" )
 
+Schema.IsCombineFacton = {
+	FACTION_CP,
+	FACTION_OW
+}
+
 local META = FindMetaTable( "Player" )
 
 function Schema:PlayerIsCombine( pl )
-	return table.HasValue( {
-		FACTION_CP,
-		FACTION_OW
-	}, pl:Team( ) )
+	return table.HasValue( self.IsCombineFacton, pl:Team( ) )
 end
 
 function META:PlayerIsCombine( )
