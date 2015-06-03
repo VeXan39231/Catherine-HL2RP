27167daddd4cff52e2bcf5bddbbccf682a04a8a1
VeXan39231 <parkerno552@yahoo.com>
2015-06-02 20:00:06 -0500

    Massive Updates
    Changed many phrases
    Fixed some grammatical issues
    Changed descriptions
    
    Add me to talk. http://www.steamcommunity.com/id/smellywetsock

--- gamemode/schema/language/english.lua
+++ gamemode/schema/language/english.lua
@@ -18,7 +18,7 @@ along with Catherine.  If not, see <http://www.gnu.org/licenses/>.
 
 catherine.language.Merge( "english", {
 	// Basic
-	[ "Basic_Schema_Title" ] = "Half - Life 2 Role Play",
+	[ "Basic_Schema_Title" ] = "Half-Life 2 Role Play",
 	[ "Basic_Schema_Desc" ] = "This place is safe.",
 	[ "Basic_Schema_IntroTitle" ] = "City 18, 2016",
 	[ "Basic_Schema_IntroDesc" ] = "This place is safe.",
@@ -30,14 +30,14 @@ catherine.language.Merge( "english", {
 	
 	// Faction Name
 	[ "Faction_Name_Citizen" ] = "Citizen",
-	[ "Faction_Name_Administrator" ] = "Administrator",
+	[ "Faction_Name_Administrator" ] = "City Administrator",
 	[ "Faction_Name_OW" ] = "Overwatch Transhuman Arm",
 	[ "Faction_Name_CP" ] = "Civil Protection",
 	
 	// Faction Description
-	[ "Faction_Desc_Citizen" ] = "He wears a dirty shirt.",
-	[ "Faction_Desc_Administrator" ] = "A city administrator.",
-	[ "Faction_Desc_OW" ] = "Mechanised Infantry units utilized to combat the ongoing insurgency, these units are the elite of the combine's forces.",
+	[ "Faction_Desc_Citizen" ] = "This citizen wears a worn and dirty blue shirt.",
+	[ "Faction_Desc_Administrator" ] = "The City Administrator, ruler of the area.",
+	[ "Faction_Desc_OW" ] = "Mechanised Infantry units utilized to combat the ongoing insurgency, these units are the elite of the Combine's forces.",
 	[ "Faction_Desc_CP" ] = "These units are the brunt of the combine's forces. These units are well-trained military units but they also take part policing the population.",
 
 	// Item
@@ -45,17 +45,17 @@ catherine.language.Merge( "english", {
 	[ "Item_FuncStr01_Wallet" ] = "Take " .. catherine.configs.cashName,
 	[ "Item_FuncStr02_Wallet" ] = "Drop " .. catherine.configs.cashName,
 	
-	[ "Item_Name_LargeBag" ] = "Large bag",
+	[ "Item_Name_LargeBag" ] = "A large, torn sack.",
 	[ "Item_Desc_LargeBag" ] = "This item adds more inventory space!",
 	
-	[ "Item_Name_SmallBag" ] = "Small bag",
+	[ "Item_Name_SmallBag" ] = "A small, worn backpack.",
 	[ "Item_Desc_SmallBag" ] = "This item adds more inventory space!",
 	
 	[ "Item_Name_CID" ] = "Citizen ID Card",
 	[ "Item_Desc_CID" ] = "The Citizen ID Card.",
 	
 	[ "Item_Name_PR" ] = "Portable Radio",
-	[ "Item_Desc_PR" ] = "Communication to other humans.",
+	[ "Item_Desc_PR" ] = "Small mechanical device used for long range communication.",
 	[ "Item_FuncStr01_PR" ] = "Set Frequency",
 	[ "Item_FuncStr02_PR" ] = "Toggle",
 	[ "Item_DataStr01_PR" ] = "Frequency",
@@ -63,37 +63,37 @@ catherine.language.Merge( "english", {
 	[ "Item_DataStr02_On_PR" ] = "On",
 	[ "Item_DataStr02_Off_PR" ] = "Off",
 	
-	[ "Item_RadioFreqQ_PR" ] = "Would you like to setting frequency for radio?",
+	[ "Item_RadioFreqQ_PR" ] = "Set your radio's frequency.",
 	[ "Item_Notify_FreqSet_PR" ] = "You are set portable radio frequency to %s.",
 	[ "Item_Notify_Error01_PR" ] = "Radio frequency is must be 101.1 ~ 199.9!",
 	[ "Item_Notify_Error02_PR" ] = "Radio frequency is must be XXX.X!",
 	
 	[ "Item_Name_RD" ] = "Request Device",
-	[ "Item_Desc_RD" ] = "If you have this, your can request it to combine.",
+	[ "Item_Desc_RD" ] = "Small device used to send messages to the Combine.",
 
 	[ "Item_Name_SC" ] = "Spray Can",
-	[ "Item_Desc_SC" ] = "If you have this, your can spray it to wall.",
+	[ "Item_Desc_SC" ] = "Can full of spray paint, use it to tag walls.",
 	
 	[ "Item_Name_BW" ] = "Breen Water",
-	[ "Item_Desc_BW" ] = "Shake and eat!",
+	[ "Item_Desc_BW" ] = "A blue can full of god knows what.",
 	
 	[ "Item_Name_RBW" ] = "Red Breen Water",
-	[ "Item_Desc_RBW" ] = "Shake and eat!",
+	[ "Item_Desc_RBW" ] = "A red can full of god knows what.",
 	
 	[ "Item_Name_GBW" ] = "Golden Breen Water",
-	[ "Item_Desc_GBW" ] = "Shake and eat!",
+	[ "Item_Desc_GBW" ] = "A golden can filed with 'water'.",
 	
 	[ "Item_Name_9MM" ] = ".9mm Pistol",
-	[ "Item_Desc_9MM" ] = "A standard .9mm Pistol.",
+	[ "Item_Desc_9MM" ] = "A grey .9mm firearm.",
 	
-	[ "Item_Name_AR2" ] = "AR2",
-	[ "Item_Desc_AR2" ] = "The corp combine.",
+	[ "Item_Name_AR2" ] = "OSIPR",
+	[ "Item_Desc_AR2" ] = "Overwatch Standard Issue Pulse Rifle",
 	
-	[ "Item_Name_SMG" ] = "SMG",
-	[ "Item_Desc_SMG" ] = "The simple sub machine gun.",
+	[ "Item_Name_SMG" ] = "MP7",
+	[ "Item_Desc_SMG" ] = "A small Heckler and Koch Sub Machine Gun.",
 	
 	[ "Item_Name_357" ] = ".357 Magnum",
-	[ "Item_Desc_357" ] = "A shiny pistol with 6 chambers that appear to rotate.",
+	[ "Item_Desc_357" ] = "A shiny revolver with 6 chambers that appear to rotate.",
 	
 	[ "Item_Name_CB" ] = "Crossbow",
 	[ "Item_Desc_CB" ] = "A massive crossbow that seems to have the ability to heat up metal bars.",
@@ -115,10 +115,10 @@ catherine.language.Merge( "english", {
 	[ "Item_FuncStr01_Ration" ] = "Open",
 	
 	[ "Item_Name_CitizenS" ] = "Citizen Supplements",
-	[ "Item_Desc_CitizenS" ] = "Raw nutrients with slight flavoring provided by the Combine.",
+	[ "Item_Desc_CitizenS" ] = "Raw nutrients with extremely little flavoring provided by the Combine.",
 	
 	[ "Item_Name_ZT" ] = "Zip Tie",
-	[ "Item_Desc_ZT" ] = "Tie to player.",
+	[ "Item_Desc_ZT" ] = "Tie this person's hands.",
 	[ "Item_FuncStr01_ZT" ] = "Tie",
 
 	[ "Item_Category_Communication" ] = "Communication",
@@ -154,7 +154,7 @@ catherine.language.Merge( "english", {
 	
 	// Radio
 	[ "Item_Name_SR" ] = "Static Radio",
-	[ "Item_Desc_SR" ] = "Communication to other humans.",
+	[ "Item_Desc_SR" ] = "A box used for long distance communication.",
 	[ "Item_NoFreq" ] = "No frequency has been set.",
 	[ "Item_Freq" ] = "Frequency set to %s.",
 	[ "Item_FuncStr01_SR" ] = "Set Frequency",
@@ -181,16 +181,16 @@ catherine.language.Merge( "english", {
 	[ "CombineOverlay_Str09" ] = "Idle state ...",
 	
 	[ "CombineOverlay_Request" ] = "%s's request - %s",
-	[ "CombineOverlay_HealthFullRecovered" ] = "Vital signs recovered ...",
-	[ "CombineOverlay_TakeDmg_Local" ] = "WARNING ! Physical bodily trauma detected ...",
-	[ "CombineOverlay_TakeDmg_NoLocal" ] = "WARNING ! Unit '%s' has damaged by unknown problems ...",
-	[ "CombineOverlay_HealthRecovering" ] = "Vital signs recovering [%s%] ...",
+	[ "CombineOverlay_HealthFullRecovered" ] = "Vital signs recovering...",
+	[ "CombineOverlay_TakeDmg_Local" ] = "WARNING - Physical bodily trauma detected...",
+	[ "CombineOverlay_TakeDmg_NoLocal" ] = "WARNING - Unit '%s' has damaged by an unknown source...",
+	[ "CombineOverlay_HealthRecovering" ] = "Vital signs recovering [%s%]...",
 	[ "CombineOverlay_LocalPlayerDeath_CP" ] = "ERROR ! Shut Down - ...",
 	[ "CombineOverlay_LocalPlayerDeath_OW" ] = "Critical Error - ...",
 	[ "CombineOverlay_PlayerDeath_CP" ] = "WARNING ! Unit '%s' vital signs absent, alerting dispatch ...",
 	[ "CombineOverlay_PlayerDeath_OW" ] = "WARNING ! Overwatch Unit '%s' vital signs absent, alerting dispatch ...",
 	[ "CombineOverlay_Online" ] = "Online ...",
-	[ "CombineOverlay_RFCitizens" ] = "Refreshing citizen lists ...",
+	[ "CombineOverlay_RFCitizens" ] = "Restoring Citizen Manifests...",
 	
 	// Attribute
 	[ "Jump_Title" ] = "Jump",
